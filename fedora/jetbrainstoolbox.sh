d16d9e3f43837215d3a2859c5dd72069f2a904da
HoangHaix86 <hai.hoang.2762@gmail.com>
2023-09-24 04:24:55 +0700

    udpa tejetbraintoolbox

--- fedora/jetbrainstoolbox.sh
+++ fedora/jetbrainstoolbox.sh
@@ -4,7 +4,9 @@ VERSION_TOOLBOX="2.0.4.17212"
 
 wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz && \
     tar -xvzf ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz && \
-    mv ./jetbrains-toolbox-$VERSION_TOOLBOX ./jetbrains && \
-    mv ./jetbrains /opt && \
-    rm ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz
+    rm -rf /opt/jetbrains && \
+    mv ./jetbrains-toolbox-$VERSION_TOOLBOX /opt/jetbrains && \
+    rm ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz && \
+    sh -c "/opt/jetbrains/jetbrains-toolbox"
+
 

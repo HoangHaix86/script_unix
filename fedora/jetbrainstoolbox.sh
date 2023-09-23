43c5e2f513cabae6ff73880c9936db64a20b8cd7
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2023-09-24 03:50:51 +0700

    update jetbrains

--- fedora/jetbrainstoolbox.sh
+++ fedora/jetbrainstoolbox.sh
@@ -4,7 +4,7 @@ VERSION_TOOLBOX="2.0.4.17212"
 
 wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz && \
     tar -xvzf ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz && \
-    mv ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz ./jetbrains && \
+    mv ./jetbrains-toolbox-$VERSION_TOOLBOX ./jetbrains && \
     mv ./jetbrains /opt && \
     rm ./jetbrains-toolbox-$VERSION_TOOLBOX.tar.gz
 

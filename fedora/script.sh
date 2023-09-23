6556adaa022e8c98c422516e8ce9a385b8479b86
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2023-09-24 03:40:37 +0700

    update

--- fedora/script.sh
+++ fedora/script.sh
@@ -2,9 +2,4 @@
 
 dnf install -y xclip
 
-# Google Chrome
-dnf install fedora-workstation-repositories
-dnf config-manager --set-enabled google-chrome
-dnf install google-chrome-stable
-
-chmod +x ./docker.hs ./vscode.sh ./ibusbamboo.sh ./jetbrainstoolbox.sh
+chmod +x ./*.sh

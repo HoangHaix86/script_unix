96201934a3dcc3a410778cba972421a0252d6dff
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2023-10-22 00:40:08 -0700

    update

--- fedora/install.sh
+++ fedora/install.sh
@@ -11,5 +11,4 @@ sudo dnf install -y @development-tools
 sudo dnf install -y kernel-devel kernel-headers dkms qt5-qtx11extras elfutils-libelf-devel zlib-devel
 sudo wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
 sudo dnf install -y VirtualBox-7.0
-sudo systemctl enable vboxdrv --now
 echo "======================== Finished VirtualBox ========================"

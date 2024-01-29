3553c0255ceae8dd61c6a7848662df64f7fde9fa
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2024-01-29 21:12:03 +0700

    add new

--- ubuntu/all.sh
+++ ubuntu/all.sh
@@ -1,20 +1,35 @@
-# Clone
-mkdir ~/workspaces
-cd ~/workspaces
-git clone git@github.com:HoangHaix86/script_unix.git
+#!/bin/bash
 
-cd ~/workspaces/script_unix/ubuntu
+# ======== Docker Engine ========
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo chmod +x docker-install.sh
-sudo chmod +x install-jetbrains-toolbox.sh
-sudo chmod +x postman-install.sh
-sudo chmod +x pyenv-install.sh
-sudo chmod +x zsh-install.sh
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo bash docker-install.sh
-sudo bash install-jetbrains-toolbox.sh
-sudo bash postman-install.sh
-sudo bash pyenv-install.sh
-sudo bash zsh-install.sh
+sudo usermod -aG docker $USER
+newgrp docker
+
+# ======== VS Code ========
+
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders

6a149c58c102ca4e222e18f6b22a970b650ccddb
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2024-01-29 21:16:34 +0700

    postman

--- ubuntu/all.sh
+++ ubuntu/all.sh
@@ -33,3 +33,31 @@ rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code # or code-insiders
+
+# ======== Postman ========
+sudo apt update
+
+sudo apt install -y openssl
+
+wget https://dl.pstmn.io/download/latest/linux_64
+
+tar zxf linux_64
+
+sudo rm -rf /opt/Postman
+
+sudo mv Postman /opt
+
+cat <<EOF > /opt/Postman/app/resources/Postman.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=/opt/Postman/app/Postman %U
+Icon=/opt/Postman/app/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOF
+
+install -t ~/.local/share/applications/ /opt/Postman/app/resources/Postman.desktop
+
+rm linux_64

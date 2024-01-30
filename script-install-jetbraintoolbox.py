5a3963592d5be44edf6925a814b0a5dc192c61c9
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2024-01-30 12:06:23 +0700

    script python get link jetbrain toolbox

--- script-install-jetbraintoolbox.py
+++ script-install-jetbraintoolbox.py
@@ -2,14 +2,14 @@
 import os
 import sys
 import subprocess
+import json
 
 url = "https://data.services.jetbrains.com/products?code=TBA&release.type=release"
 
 command = ['curl', '-s', '-o', '-', url]
 result = subprocess.run(command, capture_output=True, text=True)
 data_json = result.stdout
-try:
-    link_download = data_json[0]["releases"][0]["downloads"]["linux"]
-    print(link_download)
-except:
-    pass
+data_json = json.loads(data_json)
+
+link_download = data_json[0]["releases"][0]["downloads"]["linux"]["link"]
+print(link_download)

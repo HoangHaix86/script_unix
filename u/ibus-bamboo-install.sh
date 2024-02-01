bf2fd52669f63792d66fdd07cf7837ed70bacd3b
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2024-02-01 14:49:13 +0700

    add: bamboo

---
+++ u/ibus-bamboo-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'deb http://download.opensuse.org/repositories/home:/lamlng/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:lamlng.list
+curl -fsSL https://download.opensuse.org/repositories/home:lamlng/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_lamlng.gpg > /dev/null
+sudo apt update \
+    && sudo apt install ibus-bamboo

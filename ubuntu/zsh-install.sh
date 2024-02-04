0b0097ffb647628d2bb7ab2155d755a85583f6e6
HoangHaix86 <97834309+HoangHaix86@users.noreply.github.com>
2024-02-04 21:46:39 +0700

    update: remove alias

--- ubuntu/zsh-install.sh
+++ ubuntu/zsh-install.sh
@@ -13,10 +13,6 @@ curl -sSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install
 # plugins
 
 sudo tee -a ~/.zshrc <<EOF
-alias dc="docker compose"
-alias dps="docker ps"
-alias ds="docker stop"
-
 [[ -r ~/Repos/znap/znap.zsh ]] ||
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/Repos/znap
 source ~/Repos/znap/znap.zsh
